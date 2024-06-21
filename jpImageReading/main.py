37f6f8e3bd912091127d76654b89cdd2ebb0aaa4
furkandegirmenci <92688554+furkandegirmenci@users.noreply.github.com>
2024-06-21 20:29:13 +0300

    Update main.py

--- jpImageReading/main.py
+++ jpImageReading/main.py
@@ -1,28 +1,3 @@
-print('hi')
+import Processor
 
-import ImageReader
-import SsTaker
-import Translate
-
-
-
-
-
-
-
-
-
-
-img = SsTaker.takePicture()
-text = ImageReader.image_reader(img)
-tokens = ImageReader.tokenize_japanese_sudachi(text)
-clear_tokens = ImageReader.remove_parentheses(tokens)
-complete_tokens = ImageReader.filter_hiragana_only(clear_tokens)
-print(complete_tokens)
-if(len(complete_tokens) != 0):
-    for token in complete_tokens:
-        Translate.search_japanese_word(token)
-
-
-
-print('bye')
+Processor.process()
