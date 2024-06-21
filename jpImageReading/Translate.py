07c9a01aac6435a3ab95cb3c9909fabdfbbd9bbe
furkandegirmenci <92688554+furkandegirmenci@users.noreply.github.com>
2024-06-21 20:27:07 +0300

    Update Translate.py

--- jpImageReading/Translate.py
+++ jpImageReading/Translate.py
@@ -1,25 +1,19 @@
 import requests
-import Ui
+
 
 def search_japanese_word(word):
-    search_result = api_search_word(word)  # Replace with your Japanese word of interest
+    search_result = api_search_word(word)
 
     if search_result:
-        # Print basic information about the word
+            first_entry = search_result["data"][0]
 
-            first_entry = search_result["data"][0]  # Get the first entry from the data
             japanese_word = first_entry["japanese"][0]["word"]
-            print(f"Japanese Word : {japanese_word}")
-            # Print English meanings
+
             meanings = first_entry["senses"][0]["english_definitions"]
-            print("English Meanings:")
-            for idx, meaning in enumerate(meanings, start=1):
-                print(f"{idx}. {meaning}")
 
-            Ui.initUI(japanese_word, meanings)
-
-            print("----------------------")
-
+            clean_meanings = ', '.join(meanings)
+            complete_text = f'{japanese_word}\n\n{clean_meanings}'
+            return complete_text
     else:
         print("No data fetched from Jisho API.")
 
@@ -29,7 +23,6 @@ def api_search_word(word):
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-        # Process the JSON response as needed
         return data
     else:
         print(f"Failed to fetch data from Jisho API: {response.status_code}")
