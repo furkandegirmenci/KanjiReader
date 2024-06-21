291ac4e679f4db505f38fc078965484d9a787888
furkandegirmenci <92688554+furkandegirmenci@users.noreply.github.com>
2024-06-21 17:26:33 +0300

    Delete setup.py
    does nothing

--- jpImageReading/setup.py
+++ jpImageReading/setup.py
@@ -1,12 +1,1 @@
-#pip install requests
 
-from setuptools import setup, find_packages
-
-setup(
-    name='my_project',
-    version='1.0',
-    packages=find_packages(),
-    install_requires=[
-        'requests>=2.26.0',  # Ensure you have requests library installed
-    ],
-)
