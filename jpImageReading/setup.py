8e8aed8f42f539d6c88a07ed2652a00ea27571e1
furkandegirmenci <92688554+furkandegirmenci@users.noreply.github.com>
2024-06-22 18:15:00 +0300

    Add files via upload

---
+++ jpImageReading/setup.py
@@ -0,0 +1,21 @@
+from cx_Freeze import setup, Executable
+
+# Define executables
+executables = [
+    Executable('main.py', base='Win32GUI', icon='icon.ico')
+]
+
+# Setup parameters
+setup(
+    name='YourAppName',
+    version='1.0',
+    description='Description of your app',
+    executables=executables,
+    options={
+        'build_exe': {
+            'packages': ['pynput.mouse._win32', 'pynput.keyboard._win32', 'sudachidict_core'],  # List of packages to include with the executable
+            'include_files': ['Text.ui'],     # Additional files/directories to include
+            'include_msvcr': True,   # Include MSVC runtime files (Microsoft Visual C++ Redistributable)
+        }
+    }
+)
